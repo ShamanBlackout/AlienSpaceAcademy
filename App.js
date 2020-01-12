d883aa9545e666a2d3297a569c94cbbd2a643a0b
Jabril Muhammad <ShamanBlackout@Jabrils-MacBook-Pro.local>
2020-01-11 22:10:24 -0500

    working login page

--- App.js
+++ App.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import{withAuthenticator} from 'aws-amplify-react-native'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component {
+class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
 }
 
+export default withAuthenticator(App , { includeGreetings:true} )
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- index.js
+++ index.js
@@ -2,4 +2,30 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
+import React from 'react' 
+import AWSAppSyncClient from 'aws-appsync'
+import {Rehydrated } from 'aws-appsync-react'
+import {  ApolloProvider} from 'react-apollo' 
+import Amplify from 'aws-amplify';
+import config from './aws-exports'; 
+
+Amplify.configure(config); 
+
+const client = new AWSAppSyncClient ({
+	url: config.aws_appsync_graphqlEndpoint,
+	region: config.aws_appsync_region,
+	auth: {
+		type: config.aws_appsync_authenticationType,
+		jwtToken: async () => (await Auth.currentSession()).idToken.jwtToken 
+	}
+});
+
+const AppWithProvider = () =>(
+	<ApolloProvider client ={client}>
+	<Rehydrated>
+	<App />
+	</Rehydrated>
+	</ApolloProvider>
+
+	);
 AppRegistry.registerComponent(appName, () => App);
